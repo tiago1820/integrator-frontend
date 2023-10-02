f6f93021b23eca75b069b14743324e6399db3458
tiago1820 <tiago.zdo@gmail.com>
2023-10-02 01:43:32 -0300

    Action types were created for each action.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,54 +1,56 @@
+import { ADD_FAV, REMOVE_FAV, SET_TOTAL_CHAR, SET_USER, REMOVE_USER, ORDER, FILTER, SHOW_ALL, GET_CHARACTER_DETAIL, CLEAN_DETAIL } from "./action-types";
+
 export const addFav = (character) => {
     return {
-        type: "ADD_FAV",
+        type: ADD_FAV,
         payload: character
     }
 }
 
 export const removeFav = (id) => {
     return {
-        type: "REMOVE_FAV",
+        type: REMOVE_FAV,
         payload: id
     }
 }
 
 export const setTotalChar = (total) => {
     return {
-        type: "SET_TOTAL_CHAR",
+        type: SET_TOTAL_CHAR,
         payload: total
     }
 }
 
 export const setUser = (user) => {
     return {
-        type: "SET_USER",
+        type: SET_USER,
         payload: user
     }
 }
 
 export const removeUser = () => {
     return {
-        type: "REMOVE_USER",
+        type: REMOVE_USER,
     }
 }
 
 export const orderCards = (order) => {
     return {
-        type: "ORDER",
+        type: ORDER,
         payload: order
     }
 }
 
 export const filterCards = (gender) => {
     return {
-        type: "FILTER",
+        type: FILTER,
         payload: gender
     }
 }
 
 export const showAllcharacters = () => {
     return {
-        type: "SHOW_ALL",
+        type: SHOW_ALL,
     }
 }
 
@@ -57,11 +59,11 @@ export const getCharacterDetail = (id) => {
         fetch(`https://rickandmortyapi.com/api/character/${id}`)
             .then((response) => response.json())
             .then(data => {
-                dispatch({ type: "GET_CHARACTER_DETAIL", payload: data })
+                dispatch({ type: GET_CHARACTER_DETAIL, payload: data })
             })
     }
 }
 
 export const cleanDetail = () => {
-    return { type: "CLEAN_DETAIL" };
+    return { type: CLEAN_DETAIL };
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,5 @@
+import { ADD_FAV, REMOVE_FAV, SET_TOTAL_CHAR, SET_USER, REMOVE_USER, ORDER, FILTER, SHOW_ALL, GET_CHARACTER_DETAIL, CLEAN_DETAIL } from "./action-types";
+
 const initialState = {
     myFavorites: [],
     allCharacters: [],
@@ -8,46 +10,46 @@ const initialState = {
 
 const rootReducer = (state = initialState, { type, payload }) => {
     switch (type) {
-        case "ADD_FAV":
+        case ADD_FAV:
             let copy1 = [...state.allCharacters];
             copy1.push(payload);
             return {
-                ...state, 
+                ...state,
                 myFavorites: copy1,
                 allCharacters: copy1
             }
 
-        case "REMOVE_FAV":
+        case REMOVE_FAV:
             let copy2 = state.myFavorites.filter((char) => char.id !== Number(payload));
             return {
                 ...state,
                 myFavorites: copy2
             }
 
-        case "SET_TOTAL_CHAR":
+        case SET_TOTAL_CHAR:
             return {
                 ...state,
                 totalChars: payload
             }
 
-        case "SET_USER":
+        case SET_USER:
             return {
                 ...state, user: payload
             }
 
-        case "REMOVE_USER":
+        case REMOVE_USER:
             return {
                 ...state, user: null
             }
 
-        case "FILTER":
+        case FILTER:
             let copy3 = state.allCharacters.filter((char) => char.gender === payload)
             return {
                 ...state,
                 myFavorites: copy3
             }
 
-        case "ORDER":
+        case ORDER:
             let copy4 = [...state.allCharacters].sort((a, b) => {
                 if (payload === "A") {
                     return a.id - b.id
@@ -63,19 +65,19 @@ const rootReducer = (state = initialState, { type, payload }) => {
                 myFavorites: copy4
             }
 
-        case "SHOW_ALL":
+        case SHOW_ALL:
             return {
                 ...state,
                 myFavorites: state.allCharacters
             }
-        
-        case "GET_CHARACTER_DETAIL":
+
+        case GET_CHARACTER_DETAIL:
             return {
                 ...state,
                 characterDetail: payload
             }
 
-        case "CLEAN_DETAIL":
+        case CLEAN_DETAIL:
             return {
                 ...state,
                 characterDetail: {},
