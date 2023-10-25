0131e4d97f803b2f996dd8c3eb6e7a1e8938db5b
Tiago de Oliveira <tiago@pop-os.localdomain>
2023-10-25 15:30:52 -0300

    refactoring Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,48 +7,49 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocationPathname } from '../../hooks/index';
 
 export const Card = (props) => {
-   const dispatch = useDispatch();
-   const myFavorites = useSelector(state => state.myFavorites);
+    const dispatch = useDispatch();
+    const myFavorites = useSelector(state => state.myFavorites);
 
-   const { id, name, status, species, gender, origin, image, onClose } = props;
-   const { FAVORITES } = PATHROUTES;
-   const pathname = useLocationPathname();
-   const [isFav, setIsFav] = useState(false);
+    const { id, name, status, species, gender, origin, image, onClose } = props;
+    const { FAVORITES } = PATHROUTES;
+    const pathname = useLocationPathname();
+    const [isFav, setIsFav] = useState(false);
 
-   const handleFavorite = () => {
-      isFav ? dispatch(removeFav(id)) : dispatch(addFav(props));
-      setIsFav(!isFav);
-   }
+    const handleFavorite = () => {
+        isFav ? dispatch(removeFav(id)) : dispatch(addFav(props));
+        setIsFav(!isFav);
+    }
 
-   useEffect(() => {
-      myFavorites.forEach((fav) => {
-         if (fav.id === props.id) {
-            setIsFav(true);
-         }
-      });
-   }, [myFavorites]);
+    useEffect(() => {
+        myFavorites.forEach((fav) => {
+            if (fav.id === props.id) {
+                setIsFav(true);
+            }
+        });
+    }, [myFavorites]);
 
-   return (
-      <div className={styles.link}>
-         <div className={styles.card}>
-            {isFav
-               ? (<button onClick={handleFavorite}>❤️</button>)
-               : (<button onClick={handleFavorite}>🤍</button>)}
+    return (
+        <div className={styles.link}>
+            <div className={styles.card}>
+                {isFav
+                    ? (<button className={styles.heartButton} onClick={handleFavorite}>❤️</button>)
+                    : (<button className={styles.heartButton} onClick={handleFavorite}>🤍</button>)}
+                {pathname !== FAVORITES && <button className={styles.closeButton} onClick={() => onClose(id)}>X</button>}
+                <button className={styles.charId}>{id}</button>
 
-            <img className={styles.image} src={image} alt='' />
-            <div className={styles.cardContent}>
-               <Link to={`/detail/${id}`}>
-                  <div className={styles.name}>{name}</div>
-               </Link>
-               <div>
-                  <div>{species}</div>
-                  <div>{gender}</div>
-                  <div className={styles.origin}>{origin}</div>
-               </div>
+                <img className={styles.image} src={image} alt='' />
+                <div className={styles.cardContent}>
+                    <Link to={`/detail/${id}`}>
+                        <div className={styles.name}>{name}</div>
+                    </Link>
+                    <div>
+                        <div>{species}</div>
+                        <div>{gender}</div>
+                        <div className={styles.origin}>{origin}</div>
+                    </div>
+                </div>
+                <div className={styles.status}>{status}</div>
             </div>
-            {/* {pathname !== FAVORITES && <button onClick={() => onClose(id)}>X</button>} */}
-            <div className={styles.status}>{status}</div>
-         </div>
-      </div>
-   );
+        </div>
+    );
 }
