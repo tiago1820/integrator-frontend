c2fe1dfde0320f4a0addf3bf20ff0bf0017b6467
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-01 08:09:00 -0300

    Feature component updated

--- src/site/components/Features/Features.jsx
+++ src/site/components/Features/Features.jsx
@@ -7,23 +7,23 @@ export const Features = () => {
             <div className={styles.row}>
                 <div className={`${styles.column} ${styles.featureBox}`}>
                     <i className={styles.icon}><FaHeart /></i>
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <h3>Exploración de Personajes:</h3>
+                    <p>Permite a los usuarios explorar una amplia variedad de personajes del universo de Rick and Morty, con detalles como nombres, especies, estatus vital, etc.</p>
                 </div>
                 <div className={`${styles.column} ${styles.featureBox}`}>
                     <i className={styles.icon}><FaGlobe /></i>
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <h3>Búsqueda Avanzada:</h3>
+                    <p>Ofrece a los usuarios la posibilidad de realizar búsquedas avanzadas para encontrar personajes, episodios o ubicaciones específicos según diferentes criterios (por ejemplo, nombre, temporada, etc.).</p>
                 </div>
                 <div className={`${styles.column} ${styles.featureBox}`}>
                     <i className={styles.icon}><FaGlobe /></i>
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <h3>Favoritos:</h3>
+                    <p>Permite a los usuarios guardar sus personajes favoritos en una lista personalizada para acceder fácilmente en el futuro.</p>
                 </div>
                 <div className={`${styles.column} ${styles.featureBox}`}>
                     <i className={styles.icon}><FaGlobe /></i>
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <h3>Diseño Intuitivo y Navegación Sencilla:</h3>
+                    <p>Asegúrate de que la interfaz de usuario sea fácil de usar y que la navegación sea intuitiva para que los usuarios puedan acceder rápidamente a la información que están buscando.</p>
                 </div>
             </div>
         </div>
