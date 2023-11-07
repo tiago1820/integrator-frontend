b40537e7a9b4c22d45fc3b2e3694ef679f3ef527
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-06 23:45:54 -0300

    rearranging the navbar

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -3,6 +3,8 @@ import PATHROUTES from '../../helpers/PathRoutes.helper';
 import { RandomButton, SearchBar } from '../../components';
 import { useTranslation } from 'react-i18next';
 import styles from "./Nav.module.css";
+import { FaUser } from 'react-icons/fa';
+import logo from '../../../../public/images/logo.png';
 
 export const Nav = (props) => {
     const { t } = useTranslation();
@@ -11,11 +13,20 @@ export const Nav = (props) => {
 
     return (
         <div className={styles.navContainer}>
-            <Link className={styles.navLink} to={HOME}>{t('home')}</Link>
+            <div className={styles.left}>
+                <Link className={styles.appName} to={HOME}>
+                    <img src={logo} alt="Logo" />
+                </Link>
+            </div>
+            {/* <Link className={styles.navLink} to={HOME}>{t('home')}</Link> */}
             <Link className={styles.navLink} to={ABOUT}>{t('about')}</Link>
             <Link className={styles.navLink} to={FAVORITES}>{t('favorites')}</Link>
             <RandomButton getRandomChar={getRandomChar} />
             <SearchBar onSearch={onSearch} />
+            <div className={styles.userInfo}>
+                <span>tiago.zdo@gmail.com</span>
+                <FaUser className={styles.userIcon} />
+            </div>
         </div>
     )
 }
