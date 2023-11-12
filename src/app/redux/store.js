9b16846900852e1c592415bdfa9d45d16b7a2ce3
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-12 12:41:10 -0300

    Persisting user in local storage

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,13 +1,24 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from './reducer';
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const composeEnhancers = 
+const composeEnhancers =
     (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
+const persistConfig = {
+    key: 'user',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeEnhancers(applyMiddleware(thunk))
 );
 
-export default store;
+const persistor = persistStore(store)
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../src/app/redux/store.js';
+// import store from '../src/app/redux/store.js';
+import {store, persistor} from './app/redux/store.js';
+
 import './global.css';
 import i18n from './i18n.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 import { register } from 'swiper/element/bundle';
 
@@ -20,11 +24,13 @@ import 'swiper/css/scrollbar';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
