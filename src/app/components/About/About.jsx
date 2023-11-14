abd94807d0fb4fdc9ecf83d54fb0dbd61a1269b6
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-14 15:16:08 -0300

    About component translation

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -1,22 +1,22 @@
 import styles from "./About.module.css";
+import { useTranslation } from 'react-i18next';
+
 
 export const About = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.aboutContainer}>
-      <h2>About me</h2>
+      <h2>{t('aboutTitle')}</h2>
 
-      <p>Mi nombre es Tiago Souza de Oliveira, un apasionado Fullstack Developer con una sólida formación en Javascript, HTML5, CSS3, SQL, NodeJS, ReactJS, Linux y Docker. Originario de Argentina, he trabajado en diversas áreas, destacándome como Service Desk en Yel Informatica, brindando soporte técnico bilingüe para empleados y concesionarios Stellantis en Argentina y Brasil. Mi experiencia también abarca roles como Auxiliar de Depósito en Medical Implants S.A., donde gestioné la recepción, control y despacho de instrumentos y equipos médicos.</p>
+      <p>{t('aboutP1')}</p>
 
-      <p>A lo largo de mi carrera, he cultivado habilidades especializadas en tecnologías como React.js y Firebase, así como en la implementación de prácticas de desarrollo guiado por pruebas (TDD). Mis certificaciones, que incluyen HTML desde cero y la Certificación Linux LPI Essentials, reflejan mi compromiso con la mejora continua y la excelencia en mi campo.</p>
+      <p>{t('aboutP2')}</p>
     
-      <p>Además de mi experiencia laboral, estoy inmerso en un programa de Desarrollador Web Full Stack en Henry, donde estoy ampliando mis habilidades y conocimientos. Mi enfoque va más allá de escribir código; busco contribuir a soluciones innovadoras y eficientes.</p>
+      <p>{t('aboutP3')}</p>
     
-      <p>Este proyecto es una manifestación de mi pasión por la tecnología y el desarrollo web. A través de esta plataforma, estoy emocionado de compartir mi experiencia y conocimientos en el mundo del desarrollo. Puedes contactarme a través de mi número de teléfono +541130224325, mi correo electrónico tiago.zdo@gmail.com, o conectarte conmigo en LinkedIn y GitHub.</p>
+      <p>{t('aboutP4')}</p>
     
-      <p>Gracias por visitarnos y ser parte de esta comunidad de aprendizaje y desarrollo. ¡Espero que disfrutes explorando y aprendiendo junto a nosotros!</p>
-    
-      <p>¡Saludos,</p>
-
       <p>Tiago Souza de Oliveira</p>
     </div>
   )
