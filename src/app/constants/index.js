ff12718fc446b76a502f864a06c64faf74ef32b5
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-16 00:53:06 -0300

    Test

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,3 +1,3 @@
-export const URL = "ip-172-31-32-238.sa-east-1.compute.internal:3001/rickandmorty/";
+export const URL = "ec2-15-228-58-10.sa-east-1.compute.amazonaws.com:3001/rickandmorty/";
 
 // export const URL = "http://localhost:3001/rickandmorty/";
