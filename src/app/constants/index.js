0a29ca6a1b9396ab3caa8328b30e57a7a99c7b6d
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-16 00:47:05 -0300

    URL constant updated

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,3 +1,3 @@
-// export const URL = "http://ec2-18-230-126-120.sa-east-1.compute.amazonaws.com:3001/rickandmorty/";
+export const URL = "ip-172-31-32-238.sa-east-1.compute.internal:3001/rickandmorty/";
 
-export const URL = "http://localhost:3001/rickandmorty/";
+// export const URL = "http://localhost:3001/rickandmorty/";
