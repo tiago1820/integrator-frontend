4e49ca2fc1eabd19a0105b73d4740a401bacdf0a
Tiago <tiago.zdo@gmail.com>
2024-04-13 23:06:28 -0300

    API_URL updated

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:3001/rickandmorty/";
-
 // export const URL = "http://localhost:3001/rickandmorty/";
+
+export const URL = "https://integrator-backend.onrender.com/rickandmorty/";
